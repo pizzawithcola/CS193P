501f76c4791501fb17fcb46ff500e2c97a354e37
Jamie <jl1186@duke.edu>
2023-12-06 00:11:03 -0500

    Lecture 1 demo project
    A new Xcode project for the lecture demo

---
+++ Lec1_Memorize/Lec1_Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lec1_Memorize
+//
+//  Created by Jamie on 2023/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lec1_Memorize/Lec1_Memorize/Lec1_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lec1_MemorizeApp.swift
+//  Lec1_Memorize
+//
+//  Created by Jamie on 2023/12/6.
+//
+
+import SwiftUI
+
+@main
+struct Lec1_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
